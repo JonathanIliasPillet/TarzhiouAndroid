efbf585ff15ed1411bb5c23244a7a5a3f07b9045
Jonathan ILIAS-PILLET <jonathan.ilias@eseo.fr>
2011-04-04 22:48:45 +0200

    suppression des exceptions

--- net/chouppy/tarzhiou/exceptions/RuleViolationException.java
+++
@@ -1,7 +0,0 @@
-package net.chouppy.tarzhiou.exceptions;
-
-public class RuleViolationException extends Exception {
-	public RuleViolationException (String msg) {
-		super (msg);
-	}
-}
--- net/chouppy/tarzhiou/exceptions/VoidPlayerCannotPlay.java
+++
@@ -1,8 +0,0 @@
-package net.chouppy.tarzhiou.exceptions;
-
-public class VoidPlayerCannotPlay extends RuleViolationException {
-	public VoidPlayerCannotPlay ()
-	{
-		super ("Rule violation : a piece with no owner has been added ");
-	}
-}
