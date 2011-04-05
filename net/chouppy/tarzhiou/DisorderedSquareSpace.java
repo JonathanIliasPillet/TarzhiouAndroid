71c4c965ac57865a7d6f8b95ffc68afa9881e617
Jonathan ILIAS-PILLET <jonathan.ilias@eseo.fr>
2011-04-05 08:02:02 +0200

    code formating

--- net/chouppy/tarzhiou/DisorderedSquareSpace.java
+++ net/chouppy/tarzhiou/DisorderedSquareSpace.java
@@ -3,20 +3,20 @@ package net.chouppy.tarzhiou;
 import java.util.Iterator;
 
 public class DisorderedSquareSpace extends SquareSpace {
-	
+
 	private boolean continue_bursts;
-	
+
 	@Override
 	public boolean do_one_burst_increment() {
 		boolean result = false;
-		
+
 		for (Square current_square : squares) {
 			if (current_square.is_overloaded()) {
 				current_square.burst();
 				result = true;
 			}
 		}
-	
+
 		return result;
 	}
 
@@ -25,24 +25,23 @@ public class DisorderedSquareSpace extends SquareSpace {
 		Square current_square;
 		// tells if there is at least one burst in a turn
 		boolean at_least_one_burst = false;
-		
+
 		continue_bursts = true;
 		do {
 			Iterator<Square> i = squares.iterator();
 			at_least_one_burst = false;
 			while (i.hasNext() && continue_bursts) {
-				current_square = i.next ();
+				current_square = i.next();
 				if (current_square.is_overloaded()) {
 					current_square.burst();
 					at_least_one_burst = true;
 				}
 			}
-		}
-		while (continue_bursts && at_least_one_burst);
+		} while (continue_bursts && at_least_one_burst);
 	}
 
 	@Override
 	public void stop_doing_all_bursts() {
-		continue_bursts = false;		
+		continue_bursts = false;
 	}
 }
