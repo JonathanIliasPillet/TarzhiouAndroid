c10418e4394d1c8aca97f7f1cc99423fe69a410a
Jonathan ILIAS-PILLET <jonathan.ilias@eseo.fr>
2011-04-05 08:32:42 +0200

    starting Game class

--- net/chouppy/tarzhiou/Game.java
+++ net/chouppy/tarzhiou/Game.java
@@ -1,24 +1,36 @@
 package net.chouppy.tarzhiou;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public abstract class Game {
-	protected SquareSpace square_space;
+	protected RunnableSquareSpace square_space;
 	protected Set<Player> players;
 	protected Set<Piece> pieces;
 	
-	public SquareSpace get_square_space ()
-	{
+	protected Game () {
+		players = new LinkedHashSet<Player>();
+		pieces = new LinkedHashSet<Piece>();
+	}
+	
+	public ReadOnlySquareSpace get_square_space_view () {
 		return square_space;
 	}
 	
-	public Set<Player> get_all_players ()
-	{
-		return players;
+	public boolean play (Square this_square, Player this_player) {
+		boolean result;
+		
+		if (this_square.get_pieces_owner().equals(this_player)) {
+			this_square.add_piece(this_player.new_piece());
+			result = true;
+		}
+		else
+			result = false;
+		
+		return result;
 	}
 	
-	public Set<Piece> get_all_pieces ()
-	{
-		return pieces;
+	public boolean play (SquareKey this_square, Player this_player) {
+		return play (square_space.get_square_from_key(this_square), this_player);
 	}
 }
