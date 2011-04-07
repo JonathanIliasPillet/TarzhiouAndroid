e913f1cc86f76c550a08a7841203150d16a5f49f
Jonathan ILIAS-PILLET <jonathan.ilias@eseo.fr>
2011-04-07 07:41:02 +0200

    finished Player unit tests

--- tests/net/chouppy/tarzhiou/PlayerTest.java
+++ tests/net/chouppy/tarzhiou/PlayerTest.java
@@ -1,12 +1,25 @@
 package net.chouppy.tarzhiou;
 
+import net.chouppy.tarzhiou.listeners.PlayerListener;
 import junit.framework.TestCase;
 
-public class PlayerTest extends TestCase {
+public class PlayerTest extends TestCase implements PlayerListener {
 
 	private static final String PLAYER_NAME = "tested_player";
 	
+	/*
+	 * tested object
+	 */
 	private Player tested_player;
+	
+	/*
+	 * listener flags back
+	 */
+	private boolean listener_new_trace;
+	private boolean listener_win_trace;
+	private boolean listener_loose_trace;
+	private Player listener_me;
+	private ReadOnlyPiece listener_this_piece;
 	
 	protected void setUp() throws Exception {
 		tested_player = new Player (PlayerTest.PLAYER_NAME);
@@ -17,15 +30,34 @@ public class PlayerTest extends TestCase {
 		assertSame (PlayerTest.PLAYER_NAME, tested_player.get_name());
 	}
 	
+	public void test_new_piece ()
+	{
+		assertEquals(0, tested_player.get_pieces_count());
+		
+		Piece p = tested_player.new_piece();
+		
+		assertSame (p.get_owner(), tested_player);
+		assertEquals(1, tested_player.get_pieces_count());
+		
+		tested_player.new_piece();
+		tested_player.new_piece();
+		assertEquals(3, tested_player.get_pieces_count());
+	}
+	
 	public void test_alive ()
 	{
 		assertTrue (tested_player.is_alive());
 		
-		Piece p = tested_player.new_piece();
+		Piece new_piece = tested_player.new_piece();
 		assertTrue (tested_player.is_alive());
 		
-		tested_player.loose_a_piece(p);
+		Piece piece_won = new Piece (new Player ("other"));
+		tested_player.win_a_piece(piece_won);
+		assertTrue(tested_player.is_alive());
 		
+		tested_player.loose_a_piece(new_piece);
+		assertTrue(tested_player.is_alive());
+		tested_player.loose_a_piece(piece_won);
 		assertFalse (tested_player.is_alive());
 	}
 	
@@ -38,7 +70,73 @@ public class PlayerTest extends TestCase {
 		tested_player.retract();
 		
 		assertFalse(tested_player.is_alive());
-		//assertEquals(1, tested_player.get_pieces_count());
+		assertEquals(1, tested_player.get_pieces_count());
+	}
+	
+	public void test_equals ()
+	{
+		Player tintin = tested_player;
+		assertTrue (tested_player.equals(tintin));
+		
+		Player other = new Player ("I'm not you");
+		assertFalse(other.equals(tested_player));
+	}
+	
+	public void test_listener ()
+	{
+		tested_player.set_listener(this);
+		
+		listener_win_trace = false;
+		listener_loose_trace = false;
+		listener_new_trace = false;
+		Piece piece_won = new Piece (tested_player);
+		tested_player.win_a_piece(piece_won);
+		assertTrue (listener_win_trace);
+		assertFalse (listener_new_trace);
+		assertFalse (listener_loose_trace);
+		assertSame (tested_player, listener_me);
+		assertSame (piece_won, listener_this_piece);
+		
+		listener_win_trace = false;
+		listener_loose_trace = false;
+		listener_new_trace = false;
+		Piece new_piece = tested_player.new_piece();
+		assertTrue (listener_new_trace);
+		assertFalse(listener_win_trace);
+		assertFalse(listener_loose_trace);
+		assertSame (tested_player, listener_me);
+		assertSame (new_piece, listener_this_piece);
+		
+		listener_win_trace = false;
+		listener_loose_trace = false;
+		listener_new_trace = false;
+		tested_player.loose_a_piece(new_piece);
+		assertTrue (listener_loose_trace);
+		assertFalse(listener_win_trace);
+		assertFalse(listener_new_trace);
+		assertSame (tested_player, listener_me);
+		assertSame (new_piece, listener_this_piece);
+	}
+
+	@Override
+	public void on_loose_a_piece(Player me, ReadOnlyPiece thisPiece) {
+		listener_loose_trace = true;
+		listener_me = me;
+		listener_this_piece = thisPiece;		
+	}
+
+	@Override
+	public void on_new_piece(Player me, ReadOnlyPiece thisPiece) {
+		listener_new_trace = true;
+		listener_me = me;
+		listener_this_piece = thisPiece;	
+	}
+
+	@Override
+	public void on_win_a_piece(Player me, ReadOnlyPiece thisPiece) {
+		listener_win_trace = true;
+		listener_me = me;
+		listener_this_piece = thisPiece;	
 	}
 
 }
