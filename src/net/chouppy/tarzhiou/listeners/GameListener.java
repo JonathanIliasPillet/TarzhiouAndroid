aade8765f19987bd6354c6d04e395d2f9b63bfbf
Jonathan ILIAS-PILLET <jonathan.ilias@eseo.fr>
2011-04-20 17:15:25 +0200

    Splitted Tarzhiou in multiple projects. This one is an Android implementation of this game.

--- src/net/chouppy/tarzhiou/listeners/GameListener.java
+++
@@ -1,9 +0,0 @@
-package net.chouppy.tarzhiou.listeners;
-
-import net.chouppy.tarzhiou.Game;
-import net.chouppy.tarzhiou.Player;
-
-public interface GameListener
-{
-  void onWin(Game thisGame, Player winner);
-}
--- src/net/chouppy/tarzhiou/listeners/LinkeableCellListener.java
+++
@@ -1,7 +0,0 @@
-package net.chouppy.tarzhiou.listeners;
-
-import net.chouppy.tarzhiou.LinkeableCell;
-
-public interface LinkeableCellListener {
-	void onLink (LinkeableCell me, LinkeableCell other);
-}
