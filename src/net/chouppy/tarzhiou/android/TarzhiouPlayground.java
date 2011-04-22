6f0a0daeda1266f2da9e07677910cbef5549a0d8
Jonathan ILIAS-PILLET <ismael@chouppy.net>
2011-04-22 08:14:06 +0200

    some other trys

--- src/net/chouppy/tarzhiou/android/TarzhiouPlayground.java
+++ src/net/chouppy/tarzhiou/android/TarzhiouPlayground.java
@@ -15,9 +15,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
-public class TarzhiouPlayground extends Activity implements CellListener
+public class TarzhiouPlayground extends Activity
 {
   private AndroidTarzhiouGame myGame;
   
@@ -32,10 +31,10 @@ public class TarzhiouPlayground extends Activity implements CellListener
     myGame.createPlayer("player 1");
     myGame.createPlayer("player 2");
     
-    for (Cell aCell : myGame.getCellSpaceView().getReadOnlyCells())
-    {
-      aCell.addListener(this);
-    }
+//    for (Cell aCell : myGame.getCellSpaceView().getReadOnlyCells())
+//    {
+//      aCell.addListener(this);
+//    }
     
     myGame.start ();
     
@@ -44,44 +43,14 @@ public class TarzhiouPlayground extends Activity implements CellListener
 
   public void onCellClick (View source)
   {
-
-    NameCellKey cellKey;
+    assert source instanceof CellView;
     
-    switch (source.getId())
-    {
-      case R.id.cell1: cellKey = new NameCellKey("c1"); break;
-      case R.id.cell2: cellKey = new NameCellKey("c2"); break;
-      case R.id.cell3: cellKey = new NameCellKey("c3"); break;
-      case R.id.cell4: cellKey = new NameCellKey("c4"); break;
-      case R.id.cell5: cellKey = new NameCellKey("c5"); break;
-      case R.id.cell6: cellKey = new NameCellKey("c6"); break;
-      case R.id.cell7: cellKey = new NameCellKey("c7"); break;
-      case R.id.cell8: cellKey = new NameCellKey("c8"); break;
-      case R.id.cell9: cellKey = new NameCellKey("c9"); break;
-      case R.id.cell10: cellKey = new NameCellKey("c10"); break;
-      default: cellKey = null;
-    }
-    
+    NameCellKey cellKey = ((CellView)source).getCellKey();
+        
     if (cellKey != null)
     {
       myGame.play(cellKey, myGame.getCurrentPlayer());
     }
-  }
-
-  public void onAddPiece(Cell me, Piece thisPiece)
-  {
-    updateCell(me);
-  }
-
-  public void onBurst(Cell me)
-  {
-    updateCell(me);
-    for (Cell neighbor : me.getNeighbors())
-      updateCell(neighbor);/*
-    try {
-      Thread.sleep(500);
-    }
-    catch (Exception e){}*/
   }
   
   private ImageView getCellViewFromKey (NameCellKey thisKey)
@@ -110,25 +79,5 @@ public class TarzhiouPlayground extends Activity implements CellListener
       result = (ImageView)findViewById(R.id.cell10);
     
     return result;
-  }
-  
-  private void updateCell (Cell thisCell)
-  {
-    ImageView cellView = getCellViewFromKey ((NameCellKey)thisCell.getKey());
-    
-    switch (thisCell.getPiecesCount())
-    {
-      case 0: cellView.setImageResource(R.drawable.cell); break;
-      case 1: cellView.setImageResource(R.drawable.cell1); break;
-      case 2: cellView.setImageResource(R.drawable.cell2); break;
-      case 3: cellView.setImageResource(R.drawable.cell3); break;
-      case 4: cellView.setImageResource(R.drawable.cell4); break;
-      case 5: cellView.setImageResource(R.drawable.cell5); break;
-      case 6: cellView.setImageResource(R.drawable.cell6); break;
-      case 7: cellView.setImageResource(R.drawable.cell7); break;
-      default: cellView.setImageResource(R.drawable.cell7);
-    }
-    
-    cellView.invalidate();
   }
 }
--- src/net/chouppy/tarzhiou/android/view/CellView.java
+++ src/net/chouppy/tarzhiou/android/view/CellView.java
@@ -1,22 +1,45 @@
 package net.chouppy.tarzhiou.android.view;
 
+import net.chouppy.tarzhiou.Cell;
+import net.chouppy.tarzhiou.NameCellKey;
+import net.chouppy.tarzhiou.Piece;
+import net.chouppy.tarzhiou.Player;
+import net.chouppy.tarzhiou.listeners.CellListener;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-public class CellView extends ImageView
+public class CellView extends ImageView implements CellListener
 {
-  private String my_name;
+  private NameCellKey myName;
   
   public CellView (Context context, AttributeSet attrs)
   {
     super (context, attrs);
     
-    my_name = attrs.getAttributeValue(null, "name");
+    myName = new NameCellKey (attrs.getAttributeValue(null, "cellname"));
   }
   
-  public String getName ()
+  public NameCellKey getCellKey ()
   {
-    return my_name;
+    return myName;
+  }
+
+  public void onAddPiece(Cell me, Piece thisPiece)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void onBurst(Cell me)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+  public void onGetPiece(Cell me, Player oldPlayer, Player newPlayer)
+  {
+    // TODO Auto-generated method stub
+    
   }
 }
